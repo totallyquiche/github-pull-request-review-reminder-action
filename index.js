ec641a0f0a54bd4e9fbf581e759b339e30186783
Brian Dady <bndady@gmail.com>
2023-04-26 18:54:54 -0500

    Utilizes Octokit api to build login/timestamp

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
-const core = require('@actions/core');
+const { Octokit } = require("@octokit/rest");
 
-const { GITHUB_TOKEN, GITHUB_REPOSITORY, GITHUB_API_URL } = process.env;
-const PULL_REQUESTS_URL = `${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/pulls`;
+const { GITHUB_TOKEN, GITHUB_OWNER, GITHUB_REPOSITORY } = process.env;
 
-async function getPullRequests() {
-  return fetch(PULL_REQUESTS_URL, {
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN}`,
-    },
-  })
+const octokit = new Octokit({ auth: `${GITHUB_TOKEN}` });
+
+const getPullRequests = async () => {
+  const RESPONSE = await octokit.paginate(
+    octokit.rest.pulls.list,
+    {
+      owner: `${GITHUB_OWNER}`,
+      repo: `${GITHUB_REPOSITORY}`,
+    }
+  );
+
+  return await RESPONSE;
 }
 
-core.info(await getPullRequests());
+const getTimelineActivity = async (issueNumber) => {
+  const TIMELINE_ACTIVITY = await octokit.paginate(
+    octokit.rest.issues.listEventsForTimeline,
+    {
+      owner: `${GITHUB_OWNER}`,
+      repo: `${GITHUB_REPOSITORY}`,
+      issue_number: `${issueNumber}`,
+    }
+  );
+
+  return await TIMELINE_ACTIVITY;
+}
+
+(async () => {
+  const PULL_REQUESTS = await getPullRequests();
+
+  for (const PULL_REQUEST of PULL_REQUESTS) {
+    let REQUESTED_REVIEWERS = PULL_REQUEST.requested_reviewers;
+
+    const TIMELINE_ACTIVITY = (await getTimelineActivity(PULL_REQUEST.number))
+      .filter(timelineActivity => timelineActivity.event === 'review_requested');
+
+    REQUESTED_REVIEWERS.forEach(requestedReviewer => {
+      const REQUESTED_REVIEWER_LOGIN = requestedReviewer.login;;
+      const TIMESTAMP = TIMELINE_ACTIVITY
+        .filter(timelineActivity => {
+          return timelineActivity.requested_reviewer.login === REQUESTED_REVIEWER_LOGIN;
+        })[0].created_at
+
+      // TODO - send a notification for login/timestamp
+    });
+  }
+})();
