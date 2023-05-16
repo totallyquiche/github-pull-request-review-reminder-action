c9f88fedf4012364450d4d0990b70819640538e0
Brian Dady <bndady@gmail.com>
2023-05-16 12:14:48 -0500

    Typo

--- action.php
+++ action.php
@@ -10,7 +10,7 @@ define('GITHUB_OWNER_NAME', explode('/', GITHUB_REPOSITORY)[0]);
 define('GITHUB_REPOSITORY_NAME', explode('/', GITHUB_REPOSITORY)[1]);
 define('HOURS_UNTIL_REMINDER', intval(getenv('INPUT_HOURS_UNTIL_REMINDER')));
 define('SMTP_USERNAME', getenv('INPUT_SMTP_USERNAME'));
-define('SMTP_PASSWORD', getenv('INPUT_SMTP_PSSWORD'));
+define('SMTP_PASSWORD', getenv('INPUT_SMTP_PASSWORD'));
 
 $reminder_data = getReminderData(getGitHubClient(GITHUB_ACCESS_TOKEN),
                                  GITHUB_OWNER_NAME,
